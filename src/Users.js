a409baa0ae58cf4a2a5b4e476f69cf491d78d2cd
jannieli <32920319+jannieli@users.noreply.github.com>
2019-01-03 12:32:59 -0500

    everything except node modules

---
+++ src/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import User from './User';
+
+export default ({ users, deleteUser, goToEditMode, editing, edit })=> {
+  return (
+    <ul className='list-group'>
+      {
+        users.map( user => <User deleteUser={ deleteUser } key={ user.id } user={ user } goToEditMode={ goToEditMode } editing={ editing } cancel={()=> goToEditMode('')} edit={ edit }/>)
+      }
+    </ul>
+  );
+};
